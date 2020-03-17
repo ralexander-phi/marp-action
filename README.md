c8e32041a547c879435a3f97fef85128e531f183
Robert Alexander <ralexander.phi@gmail.com>
2020-03-17 19:52:28 -0400

    Working (#1)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # marp-action
+
 Build a Marp Presentation Website from a repo
+
+
+## Inputs
+
+* MARP\_ARGS - Build arguements for Marp
+* GITHUB\_ACTOR - Who is performing actions (I.E. Jenkins)
+* GITHUB\_REPOSITORY - The repository to push to
+* GITHUB\_TOKEN - A GitHub application access token to use
+
+
+## Example
+
+    MARP\_ARGS=slides.md -o index.html
+
+
+## Local testing
+
+docker build -t marp-action ./
+docker run -t marp-action
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+EMOJI_WORKING=(
+    "👷 Working..."
+    "🏃 Running..."
+    "🏗 Building..."
+    "🚧 Building..."
+)
+STARTING_MESSAGE=${EMOJI_WORKING[$[$RANDOM % ${#EMOJI_WORKING[@]}]]}
+
+EMOJI_SUCCESS=(
+    "💯 Done!"
+    "🚢 Ship it!"
+    "🚀 Awesome!"
+  )
+SUCCESS_MESSAGE=${EMOJI_SUCCESS[$[$RANDOM % ${#EMOJI_SUCCESS[@]}]]}
+
+echo "$STARTING_MESSAGE"
+echo ""
+
+echo "   Building with marp..."
+echo ""
+marp ${MARP_ARGS}
+echo "✔  Built Successfully!"
+echo ""
+
+echo "   Publishing to ${GITHUB_REPOSITORY} ${REMOTE_BRANCH}..."
+echo ""
+
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+git init && \
+git config user.name "marp-action" && \
+git config user.email "marp-action@users.noreply.github.com" && \
+git add . && \
+git status && \
+git commit -m'action build' && \
+git push --force $remote_repo master:${PUBLISH_TO_BRANCH}
+
+echo "✔  Pushed Successfully!"
+echo ""
+
+echo "$SUCCESS_MESSAGE"
+
